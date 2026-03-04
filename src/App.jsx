7d368b037035d677a9179d96b283a250ed32ac03
Rifah-san <rifahsanjida018@gmail.com>
2026-03-04 18:57:22 +0600

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/navbar/footer/footer"
+import Nav from "./components/navbar/Nav"
+
+function App() {
+ 
+  return (
+    <>
+      <Nav></Nav>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Nav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <div class="navbar bg-base-100 shadow-sm lg:px-20">
+        <div class="navbar-start">
+          <a class="btn btn-ghost text-xl">CS-Ticket System</a>
+        </div>
+        <div class="navbar-end flex align-middle gap-5">
+          <a href="">Home</a>
+          <a href="">FAQ</a>
+          <a href="">Changelog</a>
+          <a href="">Blog</a>
+          <a href="">Contact</a>
+          <a href="">Download</a>
+          <a class="btn bg-linear-to-tl from-green-600 to-purple-600 text-white border-0 rounded-sm hover:text-black">+ New Ticket</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
